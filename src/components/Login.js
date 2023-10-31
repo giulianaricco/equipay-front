2376954b9fc9d909a46e684ea5dab6024ea20479
giulianaricco <giu.ricco@hotmail.com>
2023-10-31 17:51:45 -0300

    Registro y login, faltan validaciones e igualar a figma

---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './style.css';
+
+//faltan validaciones
+//falta hacer igual figma
+
+function Login() {
+  return (
+    <section className="vh-100 gradient-custom" style={{ height: '100vh' }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5 h-80">
+            <div className="card bg-verde text-white" style={{ borderRadius: '1rem' }}>
+              <div className="card-body p-5 text-center">
+                <form method="post" class="mb-md-5 mt-md-4 pb-2">
+                    <h1 class="fw-bold mb-5 text-uppercase">EquiPay</h1>
+                    <h3 class="fw-bold mb-2 text-uppercase">Inicio de sesión</h3>
+    
+                    <div class="form-outline form-white mb-4">
+                    <input name="username" type="text" id="typeEmailX" placeholder= "Email" class="form-control form-control-lg" />
+                    <label class="form-label" for="typeEmailX"></label>
+                    </div>
+    
+                    <div class="form-outline form-white mb-4">
+                    <input name="password" type="password" id="typePasswordX" placeholder= "Contraseña" class="form-control form-control-lg" />
+                    <label class="form-label" for="typePasswordX"></label>
+                    </div>
+                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Register />
   </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <Router>
+//       <Route exact path="/" component={App} />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
