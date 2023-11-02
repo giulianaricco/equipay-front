0a341cf155badaae4b5c40e55ff3fdd9cc55392f
ludelossantos <ludelossantosp@gmail.com>
2023-11-01 21:25:17 -0300

    registro de usuario con conexion a la api, basico

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+      <div className="logo">
+        <Link to="/">Equipay</Link>
+      </div>
+      {/* Aquí puedes agregar otros elementos del encabezado */}
+    </header>
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -6,6 +6,53 @@ import './style.css';
 //falta hacer igual figma
 
 function Register() {
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault(); // Evitar el envío por defecto del formulario
+    
+        // Aquí puedes obtener los valores de los campos del formulario
+        const name = event.target.name.value;
+        const lastName = event.target.lastName.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+    
+        // Objeto con los datos a enviar a la API
+        const userData = {
+          nombre: name,
+          apellido: lastName,
+          correo: email,
+          password: password,
+        };
+    
+        try {
+          // Llamada a la API con fetch (aquí deberías reemplazar 'URL_DE_TU_API' con la URL de tu API)
+          const response = await fetch('/api/usuarios/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+          })/*
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.error('Error:', error));*/
+    
+          // Manejo de la respuesta de la API
+          if (response.ok) {
+            // La solicitud fue exitosa
+            // Aquí puedes manejar el resultado, por ejemplo, mostrar un mensaje de éxito
+            console.log('Usuario registrado exitosamente');
+          } else {
+            // La solicitud falló
+            // Aquí puedes manejar el error, por ejemplo, mostrar un mensaje de error
+            console.error('Error al registrar el usuario');
+          }
+        } catch (error) {
+          // Manejo de errores
+          console.error('Error:', error);
+        }
+      };
+
     return(
         <div>
         <Header isLoggedIn={false} isAdmin={false} />
@@ -15,16 +62,21 @@ function Register() {
             <div className="col-12 col-md-8 col-lg-6 col-xl-5 h-80">
                 <div className="card bg-verde text-white" style={{ borderRadius: '1rem' }}>
                 <div className="card-body p-5 text-center">
-                    <form method="post" class="mb-md-5 mt-md-4 pb-2">
+                    <form onSubmit={handleFormSubmit} method="post" class="mb-md-5 mt-md-4 pb-2">
                         <h3 class="fw-bold mb-4 ">Registro de usuario</h3>
                         
                         <div class="form-outline form-white mb-1">
-                        <input name="name" type="text" id="name" placeholder= "Nombre completo" class="form-control form-control-lg" />
+                        <input name="name" type="text" id="name" placeholder= "Nombre" class="form-control form-control-lg" />
                         <label class="form-label" for="name"></label>
+                        </div>
+                        
+                        <div class="form-outline form-white mb-1">
+                        <input name="lastName" type="text" id="lastName" placeholder= "Apellido" class="form-control form-control-lg" />
+                        <label class="form-label" for="lastName"></label>
                         </div>
         
                         <div class="form-outline form-white mb-1">
-                        <input name="username" type="text" id="typeEmailX" placeholder= "Email" class="form-control form-control-lg" />
+                        <input name="email" type="text" id="typeEmailX" placeholder= "Email" class="form-control form-control-lg" />
                         <label class="form-label" for="typeEmailX"></label>
                         </div>
         
