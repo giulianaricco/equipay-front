e2f2d39de4bd388351cbba9815bab3a082571b73
ludelossantos <ludelossantosp@gmail.com>
2023-11-08 16:51:59 -0300

    listar usuarios

--- src/paginas/ListarUsuarios.js
+++ src/paginas/ListarUsuarios.js
@@ -5,73 +5,106 @@ import Card from '../componentes/Card'; // Importa el componente Card
 import InputField from '../componentes/InputField';
 import axios from 'axios';
 
-const ListaGrupos = () => {
-  const [grupos, setGrupos] = useState([]);
-  const [grupoId, setGrupoId] = useState('');
-  const [grupoEncontrado, setGrupoEncontrado] = useState(null);
+const ListarUsuarios = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const [filtro, setFiltro] = useState(''); 
 
   useEffect(() => {
-    // Realizar una solicitud GET para obtener la lista de grupos
-    axios.get('http://localhost:8080/api/grupos/')
-      .then((response) => {
-        setGrupos(response.data);
-      })
-      .catch((error) => {
-        console.error('Error al obtener la lista de grupos:', error);
-      });
-  }, []);
+    const fetchUsuarios = async () => {
+      try {
+        const response = await fetch('/api/usuarios/', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setUsuarios(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
 
-  const buscarGrupoPorId = () => {
-    // Realizar una solicitud GET para buscar un grupo por ID
-    axios.get(`http://localhost:8080/api/grupos/${grupoId}`)
-      .then((response) => {
-        setGrupoEncontrado(response.data);
-      })
-      .catch((error) => {
-        console.error('Error al buscar el grupo por ID:', error);
-        setGrupoEncontrado(null); // Restablecer el resultado de la búsqueda
+    fetchUsuarios();
+  }, []); 
+
+  // Ordenar la lista de grupos por fecha de creación, nombre, apellido o correo electrónico
+  const ordenarUsuarios = (criterio) => {
+    const sortedUsuarios = [...usuarios];
+    sortedUsuarios.sort((a, b) => {
+      if (criterio === 'fechaCreacion') {
+        return new Date(a.fechaCreacion) - new Date(b.fechaCreacion);
+      } else if (criterio === 'apellido') {
+        // Ordenar por apellido y, si es nulo, por nombre
+        if (a.apellido !== null && b.apellido !== null) {
+          return a.apellido.localeCompare(b.apellido) || a.nombre.localeCompare(b.nombre);
+        } else if (a.apellido === null && b.apellido !== null) {
+          return 1; // Mover elementos con apellido nulo al final
+        } else if (a.apellido !== null && b.apellido === null) {
+          return -1; // Mover elementos con apellido nulo al final
+        } else {
+          return a.nombre.localeCompare(b.nombre); // Ordenar por nombre si ambos apellidos son nulos
+        }
+      } else if (criterio === 'nombre') {
+        return a.nombre.localeCompare(b.nombre);
+      }
+      return 0;
+    });
+    setUsuarios(sortedUsuarios);
+  };
+
+  // Filtrar y ver los usuarios bloqueados
+  const mostrarBloqueados = () => {
+    const bloqueados = usuarios.filter((usuario) => usuario.bloqueado);
+    setUsuarios(bloqueados);
+  };
+
+  
+  const resetearListaUsuarios = async () => {
+    try {
+      const response = await fetch('/api/usuarios/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setUsuarios(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
     <div>
       <Header />
-      <div style={{ marginTop: '50px' }}>
-      <div className="container">
-        <Card title="Buscar Grupo por ID">
-          <div className="form-group">
-            <InputField
-              value={grupoId}
-              onChange={(e) => setGrupoId(e.target.value)}
-              placeholder="ID del Grupo"
-            />
-            <Boton onClick={buscarGrupoPorId}>Buscar</Boton>
-          </div>
-          {grupoEncontrado && (
-            <Card title="Grupo Encontrado">
-              <p>ID: {grupoEncontrado.id}</p>
-              <p>Nombre: {grupoEncontrado.nombre}</p>
-              <p>Descripción: {grupoEncontrado.descripcion}</p>
-            </Card>
-          )}
-        </Card>
+      <div style={{ marginTop: '50px', width: '100%', padding: '0 30px' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+          <Boton onClick={() => ordenarUsuarios('fechaCreacion')}>Ordenar por Fecha de Creación</Boton>
+          <Boton onClick={() => ordenarUsuarios('apellido')}>Ordenar por Apellido</Boton>
+          <Boton onClick={() => ordenarUsuarios('nombre')}>Ordenar por Nombre</Boton>
+          <Boton onClick={mostrarBloqueados}>Mostrar Bloqueados</Boton>
+          <Boton onClick={resetearListaUsuarios}>Resetear Lista</Boton>
         </div>
-        <div style={{ marginTop: '50px' }}>
-        <Card title="Lista de Grupos">
+        <Card title="Lista de Usuarios">
           <ul>
-            {grupos.map((grupo) => (
-              <li key={grupo.id}>
-                <p>ID: {grupo.id}</p>
-                <p>Nombre: {grupo.nombre}</p>
-                <p>Descripción: {grupo.descripcion}</p>
+            {usuarios.map((usuario) => (
+              <li key={usuario.id} style={{ borderBottom: '1px solid #ccc', padding: '10px 0' }}>
+                <p>{usuario.correo}</p>
+                <p>{usuario.nombre} {usuario.apellido ? usuario.apellido : ' '}</p>
               </li>
             ))}
           </ul>
         </Card>
       </div>
     </div>
-    </div>
   );
 };
 
-export default ListaGrupos;
+export default ListarUsuarios;
