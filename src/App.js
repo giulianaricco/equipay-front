1cedd6b90794a86ac8818167be64bed9f548f565
Sofia Guerra <sofiguerra27@hotmail.com>
2023-12-12 18:39:52 -0300

    fix roles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import { AuthProvider, useAuth } from './contexto/AuthContext';
+import { useAuth } from './contexto/AuthContext';
 import PaginaInicio from './paginas/Inicio/PaginaInicio';
 import WelcomePage from './paginas/bienvenida/WelcomePage';
 import PaginaListarGrupo from './paginas/listaGrupo/ListaGrupos';
@@ -41,294 +41,297 @@ import PaginaVisualizarGrupo from './paginas/resumenesGrupo/VisualizarGrupo';
 import PaginaHistorico from './paginas/historicoActividad/HistoricoActividad';
 
 function App() {
+  const { isAuthenticated, getToken, login, user } = useAuth();
+
+  useEffect(() => {
+    if (isAuthenticated && !user) {
+      const token = getToken()
+      if (token) {
+        login(token)
+      }
+    }
+  }, []);
 
   const ProtectedRouteUser = ({ children }) => {
-    const { isAuthenticated, user } = useAuth();
-  
-    if (isAuthenticated && user.rol === 'Usuario') {
+    if (isAuthenticated && user?.rol === 'Usuario') {
       return children;
-    } 
-    else {
+    }
+    else if (!isAuthenticated) {
       return <Navigate to={"/"} />;
     }
   };
 
   const ProtectedRouteAdmin = ({ children }) => {
-    const { isAuthenticated, user } = useAuth();
-  
-    if (isAuthenticated && user.rol === 'Admin') {
+    if (isAuthenticated && user?.rol === 'Admin') {
       return children;
-    } 
-    else {
+    }
+    else if (!isAuthenticated) {
       return <Navigate to={"/"} />;
     }
   };
 
   const ProtectedRouteGenerico = ({ children }) => {
-    const { isAuthenticated, user } = useAuth();
-    console.log(isAuthenticated)
-    if (isAuthenticated && (user.rol === 'Admin' || user.rol === 'Usuario')) {
+    if (isAuthenticated && (user?.rol === 'Admin' || user?.rol === 'Usuario')) {
       return children;
-    } 
-    else {
+    }
+    else if (!isAuthenticated) {
       return <Navigate to={"/"} />;
     }
   };
-  
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-            <Route 
-              path="/" 
-              element={<PaginaInicio />} 
-              />
-            <Route 
-              path="/welcome" 
-              element={
-                <ProtectedRouteGenerico>
-                  <WelcomePage />
-                </ProtectedRouteGenerico>
-              } 
-            />
-            <Route 
-              path="/agregar-grupo" 
-              element={
-                <ProtectedRouteUser>
-                  <PaginaAgregarGrupo />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/agregar-grupo-admin" 
-              element={
-              <ProtectedRouteAdmin>
-                <PaginaAgregarGrupoAdmin />
-              </ProtectedRouteAdmin>} 
-            />
-            <Route 
-              path="/listar-grupos" 
-              element={
-                <ProtectedRouteUser>
-                  <PaginaListarGrupo/>
-                </ProtectedRouteUser>
-              }
-            />
-            <Route 
-              path="/listar-grupos-admin" 
-              element={
-                <ProtectedRouteAdmin>
-                  <PaginaListarGrupoAdmin />
-                </ProtectedRouteAdmin>
-              } 
-              />
-            <Route 
-              path="/registrar-usuario" 
-              element={
-                <PaginaRegistrarUsuario />
-              } 
-            />
-            <Route 
-              path="/listar-usuarios" 
-              element={
-                <ProtectedRouteAdmin>
-                  <PaginaListarUsuarios />
-                </ProtectedRouteAdmin>
-              } 
-            />
-            <Route 
-              path="/eliminar-categoria" 
-              element={
-                <ProtectedRouteAdmin>
-                  <PaginaEliminarCategoria />
-                </ProtectedRouteAdmin>
-              } 
-            />
-            <Route 
-              path="/registrar-gasto" 
-              element={
-                <ProtectedRouteUser>
-                  <PaginaRegistrarGasto />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/iniciar-sesion" 
-              element={
-                  <PaginaIniciarSesion />
-              } 
-            />
-            <Route 
-              path="/eliminar-usuario" 
-              element={
-                <ProtectedRouteAdmin>
-                  <PaginaEliminarUsuario />
-                </ProtectedRouteAdmin>
-              } 
-            />
-            <Route 
-              path="/bloquear-usuario" 
-              element={
-                <ProtectedRouteAdmin>
-                  <PaginaBloquearUsuario />
-                </ProtectedRouteAdmin>
-              } 
-            />
-            <Route 
-              path="/desbloquear-usuario" 
-              element={
-                <ProtectedRouteAdmin>              
-                  <PaginaDesbloquearUsuario />
-                </ProtectedRouteAdmin>
-              } 
-            />
-            <Route 
-              path="/eliminar-cuenta" 
-              element={
-                <ProtectedRouteGenerico>
-                  <PaginaEliminarCuenta />
-                </ProtectedRouteGenerico>
-              }
-            />
-            <Route 
-              path="/registrar-pago" 
-              element={
-                <ProtectedRouteUser>
-                  <PaginaRegistrarPago />
-                </ProtectedRouteUser>
-              }
-            />
-            <Route 
-              path="/consultar-deudas" 
-              element={
-                <ProtectedRouteUser>
-                  <PaginaConsultarDeuda />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/detalle-grupo/:id" 
-              element={
-                <ProtectedRouteUser>
-                  <PaginaDetalleGrupo />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/detalle-grupo-admin/:id" 
-              element={
-                <ProtectedRouteAdmin>
-                  <PaginaDetalleGrupoAdmin />
-                </ProtectedRouteAdmin>
-              } 
-            />
-            <Route 
-              path="/invitar-amigo" 
-              element={
-                <ProtectedRouteUser>             
-                  <InvitarAmigo />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/recuperar-contrasena" 
-              element={
-                  <PaginaRecuperarContrasena />
-              } 
-            />
-            <Route 
-              path="/unirse-grupo-link" 
-              element={
-                <ProtectedRouteUser>             
-                  < PaginaUnirseGrupoLink />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/unirse-grupo-codigo" 
-              element={
-                <ProtectedRouteUser>
-                  < PaginaUnirseGrupoCodigo />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/estadisticas-personales" 
-              element={
-                <ProtectedRouteUser>
-                  < PaginaVisualizarGraficosUser />
-                </ProtectedRouteUser>
-              } 
-            />
-            <Route 
-              path="/estadisticas"
-              element={
-                <ProtectedRouteAdmin>
-                  < PaginaVisualizarGraficosAdmin />
-                </ProtectedRouteAdmin>
-              } 
-            />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/Iniciar-sesion" element={<PaginaIniciarSesion />} />
 
-        <Route 
-          path="/agregar-categoria" 
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<PaginaInicio />}
+        />
+        <Route
+          path="/welcome"
+          element={
+            <ProtectedRouteGenerico>
+              <WelcomePage />
+            </ProtectedRouteGenerico>
+          }
+        />
+        <Route
+          path="/agregar-grupo"
+          element={
+            <ProtectedRouteUser>
+              <PaginaAgregarGrupo />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/agregar-grupo-admin"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaAgregarGrupoAdmin />
+            </ProtectedRouteAdmin>}
+        />
+        <Route
+          path="/listar-grupos"
+          element={
+            <ProtectedRouteUser>
+              <PaginaListarGrupo />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/listar-grupos-admin"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaListarGrupoAdmin />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/registrar-usuario"
+          element={
+            <PaginaRegistrarUsuario />
+          }
+        />
+        <Route
+          path="/listar-usuarios"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaListarUsuarios />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/eliminar-categoria"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaEliminarCategoria />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/registrar-gasto"
+          element={
+            <ProtectedRouteUser>
+              <PaginaRegistrarGasto />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/iniciar-sesion"
+          element={
+            <PaginaIniciarSesion />
+          }
+        />
+        <Route
+          path="/eliminar-usuario"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaEliminarUsuario />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/bloquear-usuario"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaBloquearUsuario />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/desbloquear-usuario"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaDesbloquearUsuario />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/eliminar-cuenta"
+          element={
+            <ProtectedRouteGenerico>
+              <PaginaEliminarCuenta />
+            </ProtectedRouteGenerico>
+          }
+        />
+        <Route
+          path="/registrar-pago"
+          element={
+            <ProtectedRouteUser>
+              <PaginaRegistrarPago />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/consultar-deudas"
+          element={
+            <ProtectedRouteUser>
+              <PaginaConsultarDeuda />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/detalle-grupo/:id"
+          element={
+            <ProtectedRouteUser>
+              <PaginaDetalleGrupo />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/detalle-grupo-admin/:id"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaDetalleGrupoAdmin />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route
+          path="/invitar-amigo"
+          element={
+            <ProtectedRouteUser>
+              <InvitarAmigo />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/recuperar-contrasena"
+          element={
+            <PaginaRecuperarContrasena />
+          }
+        />
+        <Route
+          path="/unirse-grupo-link"
+          element={
+            <ProtectedRouteUser>
+              < PaginaUnirseGrupoLink />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/unirse-grupo-codigo"
+          element={
+            <ProtectedRouteUser>
+              < PaginaUnirseGrupoCodigo />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/estadisticas-personales"
+          element={
+            <ProtectedRouteUser>
+              < PaginaVisualizarGraficosUser />
+            </ProtectedRouteUser>
+          }
+        />
+        <Route
+          path="/estadisticas"
+          element={
+            <ProtectedRouteAdmin>
+              < PaginaVisualizarGraficosAdmin />
+            </ProtectedRouteAdmin>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/Iniciar-sesion" element={<PaginaIniciarSesion />} />
+
+        <Route
+          path="/agregar-categoria"
           element={
             <ProtectedRouteAdmin>
               <PaginaAgregarCategoria />
             </ProtectedRouteAdmin>
           }
         />
-        <Route 
-          path="/modificar-categoria" 
+        <Route
+          path="/modificar-categoria"
           element={
             <ProtectedRouteAdmin>
               <PaginaModificarCategoria />
             </ProtectedRouteAdmin>
           }
         />
-        <Route 
-          path="/modificar-perfil" 
+        <Route
+          path="/modificar-perfil"
           element={
             <ProtectedRouteUser>
               <PaginaModificarPerfil />
             </ProtectedRouteUser>
-          } 
+          }
         />
-        <Route 
-          path="/modificar-perfil-admin" 
+        <Route
+          path="/modificar-perfil-admin"
           element={
             <ProtectedRouteAdmin>
               <PaginaModificarPerfilAdmin />
             </ProtectedRouteAdmin>
           }
         />
-        <Route 
-        path="/alta-usuario" 
-        element={
-          <ProtectedRouteAdmin>
-            <PaginaAltaUsuario />
-          </ProtectedRouteAdmin>
-        }
+        <Route
+          path="/alta-usuario"
+          element={
+            <ProtectedRouteAdmin>
+              <PaginaAltaUsuario />
+            </ProtectedRouteAdmin>
+          }
         />
-        <Route 
-          path="/visualizar-usuario" 
+        <Route
+          path="/visualizar-usuario"
           element={
             <ProtectedRouteAdmin>
               <PaginaVisualizarUsuario />
             </ProtectedRouteAdmin>
           }
         />
-        <Route 
-          path="/visualizar-grupo" 
+        <Route
+          path="/visualizar-grupo"
           element={
             <ProtectedRouteAdmin>
               <PaginaVisualizarGrupo />
             </ProtectedRouteAdmin>
           }
         />
-        <Route 
-          path="/historico-actividad" 
+        <Route
+          path="/historico-actividad"
           element={
             <ProtectedRouteUser>
               <PaginaHistorico />
@@ -336,8 +339,7 @@ function App() {
           }
         />
       </Routes>
-      </Router>
-    </AuthProvider>
+    </Router>
   );
 }
 
--- src/contexto/AuthContext.js
+++ src/contexto/AuthContext.js
@@ -1,10 +1,11 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { parseJwt } from '../utils/jwtUtils';
 
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+
   const storedToken = localStorage.getItem("token");
   const [token, setToken] = useState(storedToken || null);
   const [user, setUser] = useState(null);
@@ -12,18 +13,20 @@ export const AuthProvider = ({ children }) => {
 
   const processToken = (token) => {
     const decodedToken = parseJwt(token);
-    console.log(decodedToken);
+    let user = {}
     if (decodedToken) {
-      setUser({
+      user = {
         nombre: decodedToken.nombre,
         apellido: decodedToken.apellido,
         rol: decodedToken.rol,
         correo: decodedToken.sub,
-      });
+      }
+      setUser(user);
       localStorage.setItem("token", token);
     } else {
       console.error('Error al decodificar el token');
     }
+    return user
   };
 
   const login = (token) => {
@@ -43,7 +46,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, token, login, logout, getToken, isAuthenticated }}>
+    <AuthContext.Provider value={{ user, token, login, logout, getToken, isAuthenticated, processToken }}>
       {children}
     </AuthContext.Provider>
   );
